5fe11f6686f945e6692d5dd3d0c4a81cc711f1c0
want74 <want74@rambler.ru>
2022-03-24 06:22:10 +0900

    map

--- data/dots_2.js
+++ data/dots_2.js
@@ -1,1 +1,242 @@
-var json_dots_2 = {"type":"FeatureCollection","name":"dots_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"name":"Сладкая планета","dist":"1"},"geometry":{"type":"Point","coordinates":[129.717079480342903,62.008863273229551]}},{"type":"Feature","properties":{"id":null,"name":"ТКД","dist":"1"},"geometry":{"type":"Point","coordinates":[129.716326472177997,62.008600674154103]}},{"type":"Feature","properties":{"id":null,"name":"Миллениум","dist":"1"},"geometry":{"type":"Point","coordinates":[129.716373098070562,62.008427795193384]}},{"type":"Feature","properties":{"id":null,"name":"Инкотрейд","dist":"1"},"geometry":{"type":"Point","coordinates":[129.716885982888755,62.008427795193384]}},{"type":"Feature","properties":{"id":null,"name":"Лапидус","dist":"1"},"geometry":{"type":"Point","coordinates":[129.717478131724306,62.008458432042772]}},{"type":"Feature","properties":{"id":null,"name":"Аким","dist":"1"},"geometry":{"type":"Point","coordinates":[129.715962790216025,62.008088599444029]}},{"type":"Feature","properties":{"id":null,"name":"Диана","dist":"1"},"geometry":{"type":"Point","coordinates":[129.716750767800335,62.007559008849249]}},{"type":"Feature","properties":{"id":null,"name":"Анна","dist":"1"},"geometry":{"type":"Point","coordinates":[129.717548070563168,62.008082034327749]}},{"type":"Feature","properties":{"id":null,"name":"Дуэт","dist":"1"},"geometry":{"type":"Point","coordinates":[129.718541202074761,62.008226466559158]}},{"type":"Feature","properties":{"id":null,"name":"Аниган","dist":"1"},"geometry":{"type":"Point","coordinates":[129.714288920672999,62.00166067303585]}},{"type":"Feature","properties":{"id":null,"name":"Город сладостей","dist":"1"},"geometry":{"type":"Point","coordinates":[129.714116404870538,62.002398301075864]}},{"type":"Feature","properties":{"id":null,"name":"Вкуснятина","dist":"1"},"geometry":{"type":"Point","coordinates":[129.715044260132515,62.003532068399373]}},{"type":"Feature","properties":{"id":null,"name":"ТКД","dist":"2"},"geometry":{"type":"Point","coordinates":[129.711178973639107,61.99944768174538]}},{"type":"Feature","properties":{"id":null,"name":"Фуд опт","dist":"2"},"geometry":{"type":"Point","coordinates":[129.711607931850665,61.999027392238339]}},{"type":"Feature","properties":{"id":null,"name":"Оптово-розничный магазин","dist":"2"},"geometry":{"type":"Point","coordinates":[129.709402527132454,61.999373255900977]}},{"type":"Feature","properties":{"id":null,"name":"ТКД","dist":"2"},"geometry":{"type":"Point","coordinates":[129.705290123408389,61.99851515656227]}},{"type":"Feature","properties":{"id":null,"name":"Оптово-розничная фирма","dist":"2"},"geometry":{"type":"Point","coordinates":[129.702063611643013,61.996991532877608]}},{"type":"Feature","properties":{"id":null,"name":"Виконт","dist":"2"},"geometry":{"type":"Point","coordinates":[129.688439525836145,61.995931956548567]}},{"type":"Feature","properties":{"id":null,"name":"Добрый","dist":"2"},"geometry":{"type":"Point","coordinates":[129.696114147751985,62.005532473846522]}},{"type":"Feature","properties":{"id":null,"name":"СвитОпт","dist":"2"},"geometry":{"type":"Point","coordinates":[129.696076847037915,62.00596142771068]}},{"type":"Feature","properties":{"id":null,"name":"Артком","dist":"2"},"geometry":{"type":"Point","coordinates":[129.697401022386714,62.00588264071915]}}]}
+var json_dots_2 = {
+    "type": "FeatureCollection",
+    "name": "dots_2",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "id": "1",
+            "name": "Сладкая планета",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.717079480342903, 62.008863273229551]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "2",
+            "name": "ТКД",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.716326472177997, 62.008600674154103]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "3",
+            "name": "Миллениум",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.716373098070562, 62.008427795193384]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "4",
+            "name": "Инкотрейд",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.716885982888755, 62.008427795193384]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "5",
+            "name": "Лапидус",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.717478131724306, 62.008458432042772]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "6",
+            "name": "Аким",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.715962790216025, 62.008088599444029]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "7",
+            "name": "Диана",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.716750767800335, 62.007559008849249]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "8",
+            "name": "Анна",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.717548070563168, 62.008082034327749]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "9",
+            "name": "Дуэт",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.718541202074761, 62.008226466559158]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "10",
+            "name": "Аниган",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.714288920672999, 62.00166067303585]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "11",
+            "name": "Город сладостей",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.714116404870538, 62.002398301075864]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "12",
+            "name": "Вкуснятина",
+            "dist": "1"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.715044260132515, 62.003532068399373]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "13",
+            "name": "ТКД",
+            "dist": "2"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.711178973639107, 61.99944768174538]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "14",
+            "name": "Фуд опт",
+            "dist": "2"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.711607931850665, 61.999027392238339]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "15",
+            "name": "Оптово-розничный магазин",
+            "dist": "2"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.709402527132454, 61.999373255900977]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "16",
+            "name": "ТКД",
+            "dist": "2"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.705290123408389, 61.99851515656227]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "17",
+            "name": "Оптово-розничная фирма",
+            "dist": "2"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.702063611643013, 61.996991532877608]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "18",
+            "name": "Виконт",
+            "dist": "2"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.688439525836145, 61.995931956548567]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "19",
+            "name": "Добрый",
+            "dist": "2"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.696114147751985, 62.005532473846522]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "20",
+            "name": "СвитОпт",
+            "dist": "2"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.696076847037915, 62.00596142771068]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "id": "21",
+            "name": "Артком",
+            "dist": "2"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [129.697401022386714, 62.00588264071915]
+        }
+    }]
+}
